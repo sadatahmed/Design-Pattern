645927bdea87e953ccb4cadf4d393fefff02e17f
Sadat Ahmed <sadat.ahmed@bjitgroup.com>
2023-02-09 10:36:56 +0600

    Initial Commit

---
+++ DesignPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPattern
+//
+//  Created by apple on 2023/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
